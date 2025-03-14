e23bc6432e31ca09cb0b3caef59443cfab074d57
Oleg Luppov <lupperpuper@gmail.com>
2025-03-14 22:21:16 +0400

    Add component table

---
+++ src/components/table/table.ts
@@ -0,0 +1,30 @@
+import { TRenderData } from '../../shared/types/types'
+
+function renderTable(data: TRenderData[]) {
+	const tableBody = document.querySelector('.table__b') as HTMLElement
+	tableBody.textContent = ''
+
+	if (!data || !data.length) return
+
+	const tmpRow = document.querySelector('.template-table-b-r') as HTMLTemplateElement
+
+	data.forEach((user) => {
+		const row = tmpRow.content.querySelector('.table__r')!.cloneNode(true) as HTMLLIElement
+
+		const link = row.querySelector('.table__link--name') as HTMLLinkElement
+		link.textContent = user.name ?? ''
+		if (user.link) {
+			link.href = user.link
+		}
+
+		const age = row.querySelector('.table__content--age') as HTMLParagraphElement
+		age.textContent = user.age ? `${user.age}` : 'Не заполнено'
+
+		const createdAt = row.querySelector('.table__content--created-at') as HTMLParagraphElement
+		createdAt.textContent = user.createdAt
+
+		tableBody.append(row)
+	})
+}
+
+export default renderTable
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import './style.css'
+
 import Scripts from './shared/mock/methods'
+import renderTable from './components/table/table'
 
 async function renderWidget() {
 	const data = await Scripts.getData()
 
 	if (!data || !data.length) return
 
-	console.log(data)
+	console.log('renderData', data)
+	renderTable(data)
 }
 
 renderWidget()
